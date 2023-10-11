36d777f721251337d2f19725fa397cbaaf8ba926
yjinsoo <91886289+yjinsoo@users.noreply.github.com>
2023-10-11 09:51:07 +0900

    Update tomcatinit.sh

--- tomcatinit.sh
+++ tomcatinit.sh
@@ -3,4 +3,5 @@
 /usr/bin/envsubst '$MYDB' < /server.xml > /usr/local/tomcat/conf/server.xml
 /usr/bin/envsubst '$MYDB' < /account.jsp > /usr/local/tomcat/webapps/ROOT/account.jsp
 /usr/bin/envsubst '$MYDB' < /accounttest.jsp > /usr/local/tomcat/webapps/ROOT/accounttest.jsp
+/usr/bin/envsubst '$MYDB' < /surveytest.jsp > /usr/local/tomcat/webapps/ROOT/surveytest.jsp
 exec catalina.sh run
